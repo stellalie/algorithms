f1c2936b8a1d5b53b52325e6324006f4ba17b316
Stella Lie <ask@stellalie.com>
2015-02-04 18:43:13 +1100

    Initial commit

---
+++ percolation/src/Percolation.java
@@ -0,0 +1,70 @@
+public class Percolation {
+    private final int N;
+    private final int VIRTUAL_TOP_INDEX;
+    private final int VIRTUAL_BOTTOM_INDEX;
+
+    private boolean[] sites;
+    private WeightedQuickUnionUF uf;
+
+    public Percolation(int N) {
+        this.N = N;
+        VIRTUAL_TOP_INDEX = 0;
+        VIRTUAL_BOTTOM_INDEX = N * N + 1;
+
+        sites = new boolean[N * N + 2];
+        uf = new WeightedQuickUnionUF(N * N + 2);
+    }
+
+    public void open(int i, int j) {
+        int index = getIndex(i, j);
+        if (!isOpen(i, j)) {
+            // Connect to virtual top case
+            if (index <= N) {
+                uf.union(VIRTUAL_TOP_INDEX, index);
+            }
+            // Connect to virtual bottom case
+            if (index >= N * N - N + 1) {
+                uf.union(VIRTUAL_BOTTOM_INDEX, index);
+            }
+            // Connect top, bottom, left, and right sides
+            // with brute force, yea baby!
+            int[][] sides = new int[][]{
+                    {i - 1, j},    // Top side index
+                    {i + 1, j},    // Bottom side index
+                    {i, j - 1},    // Left side index
+                    {i, j + 1},    // Right side index
+            };
+            for (int[] side : sides) {
+                try {
+                    int sideIndex = getIndex(side[0], side[1]);
+                    if (sites[sideIndex]) {
+                        uf.union(sideIndex, index);
+                    }
+                } catch (IndexOutOfBoundsException ignored) {}
+            }
+            sites[index] = true;
+        }
+    }
+
+    public boolean isOpen(int i, int j) {
+        int index = getIndex(i, j);
+        return sites[index];
+    }
+
+    public boolean isFull(int i, int j) {
+        int index = getIndex(i, j);
+        return sites[index] && uf.connected(VIRTUAL_TOP_INDEX, index);
+    }
+
+    public boolean percolates() {
+        return uf.connected(VIRTUAL_TOP_INDEX, VIRTUAL_BOTTOM_INDEX);
+    }
+
+    private int getIndex(int i, int j) {
+        int index = N * (i - 1) + j;
+        if (index > N * N || index < 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return index;
+    }
+}
---
+++ percolation/src/PercolationStats.java
@@ -0,0 +1,43 @@
+public class PercolationStats {
+    private double[] results;
+
+    public PercolationStats(int N, int T) {
+        results = new double[T];
+        for (int t = 0; t < T; t++) {
+            Percolation percolation = new Percolation(N);
+            int openCount = 0;
+            while (!percolation.percolates()) {
+                int i = StdRandom.uniform(N) + 1;
+                int j = StdRandom.uniform(N) + 1;
+                if (!percolation.isOpen(i, j)) {
+                    openCount++;
+                    percolation.open(i, j);
+                }
+            }
+            results[t] = (double) openCount / (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(results);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(results);
+    }
+
+    public double confidenceLo() {
+        return mean() - (1.96 * stddev()) / Math.sqrt(results.length);
+    }
+
+    public double confidenceHi() {
+        return mean() + (1.96 * stddev()) / Math.sqrt(results.length);
+    }
+
+    public static void main(String[] args) {
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = " + stats.confidenceLo() + ", " + stats.confidenceHi());
+    }
+}
