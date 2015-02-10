9144fcb249220af96af4cad774535d0ca4078ec6
Stella Lie <ask@stellalie.com>
2015-02-10 11:40:14 +1100

    [FEATURE] Now implemented 2 UF objects to solve backwash issue. Score 100/100.

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -2,20 +2,20 @@ public class Percolation {
     private final int N;
     private final int VIRTUAL_TOP_INDEX;
     private final int VIRTUAL_BOTTOM_INDEX;
-
     private boolean[] sites;
-    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF percolatesUF;
+    private WeightedQuickUnionUF fillUF;
 
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException();
         }
-
         this.N = N;
         VIRTUAL_TOP_INDEX = 0;
         VIRTUAL_BOTTOM_INDEX = N * N + 1;
         sites = new boolean[N * N + 2];
-        uf = new WeightedQuickUnionUF(N * N + 2);
+        percolatesUF = new WeightedQuickUnionUF(N * N + 2);
+        fillUF = new WeightedQuickUnionUF(N * N + 1);
     }
 
     public void open(int i, int j) {
@@ -23,14 +23,14 @@ public class Percolation {
         if (!isOpen(i, j)) {
             // Open it!
             sites[index] = true;
-
             // Connect to virtual top case
             if (index <= N) {
-                uf.union(VIRTUAL_TOP_INDEX, index);
+                percolatesUF.union(VIRTUAL_TOP_INDEX, index);
+                fillUF.union(VIRTUAL_TOP_INDEX, index);
             }
             // Connect to virtual bottom case
             if (index >= N * N - N + 1) {
-                uf.union(VIRTUAL_BOTTOM_INDEX, index);
+                percolatesUF.union(VIRTUAL_BOTTOM_INDEX, index);
             }
             // Connect top, bottom, left, and right sides
             // with brute force, yea baby!
@@ -44,7 +44,8 @@ public class Percolation {
                 try {
                     int sideIndex = getIndex(side[0], side[1]);
                     if (sites[sideIndex]) {
-                        uf.union(sideIndex, index);
+                        percolatesUF.union(sideIndex, index);
+                        fillUF.union(sideIndex, index);
                     }
                 } catch (IndexOutOfBoundsException ignored) {}
             }
@@ -52,21 +53,19 @@ public class Percolation {
     }
 
     public boolean isOpen(int i, int j) {
-        int index = getIndex(i, j);
-        return sites[index];
+        return sites[getIndex(i, j)];
     }
 
     public boolean isFull(int i, int j) {
-        int index = getIndex(i, j);
-        return sites[index] && uf.connected(VIRTUAL_TOP_INDEX, index);
+        return fillUF.connected(VIRTUAL_TOP_INDEX, getIndex(i, j));
     }
 
     public boolean percolates() {
-        return uf.connected(VIRTUAL_TOP_INDEX, VIRTUAL_BOTTOM_INDEX);
+        return percolatesUF.connected(VIRTUAL_TOP_INDEX, VIRTUAL_BOTTOM_INDEX);
     }
 
     private int getIndex(int i, int j) {
-        if (i <= 0 || j <= 0 || j > N || j > N) {
+        if (i <= 0 || j <= 0 || i > N || j > N) {
             throw new IndexOutOfBoundsException();
         }
         return N * (i - 1) + j;
